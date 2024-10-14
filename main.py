faeec402950d47596fea61eaed916b54ea968f92
Dan1lk <prem_tank99@mail.ru>
2024-10-14 14:58:32 +0300

    добавил функцию data_migration

--- main.py
+++ main.py
@@ -117,15 +117,72 @@ def conn_and_extract_postgresql():
     sales_df.show()
 
     # Регистрируем временную таблицу:
-    sales_df.createOrReplaceGlobalTempView('sales_df')
+    sales_df.createOrReplaceTempView('sales')
 
-    # Подсчитаем общее количество продаж и сумму продаж для каждого региона и каждого продукта.
-    spark.sql("""
-                    SELECT region, count(*) as count_sales, sum(sale_amount) as sum_sale_amount
-                    FROM sales_df
-                    GROUP BY region
-                """).show()
-    
+    # Подсчитаем общее количество продаж и сумму продаж для каждого региона и каждого продукта и средний чек (average_sale_amount) по регионам и продуктам.
+    sales_by_region = spark.sql("""
+                        SELECT region, product_id, count(*) as count_sales, sum(sale_amount) as sum_sale_amount, 
+                            avg(sum(sale_amount)) OVER (PARTITION BY region) as avg_sale_amount
+                        FROM sales
+                        GROUP BY region, product_id
+                    """)
+
+    # Загрузим агрегированные данные в postgresql:
+    sales_by_region.write.mode("overwrite") \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://host.docker.internal:5432/test") \
+        .option("dbtable", "sales_by_region") \
+        .option("user", "user") \
+        .option("password", "password") \
+        .option("driver", "org.postgresql.Driver") \
+        .save()
+
+    print("Данные загружены")
+    spark.stop()
+
+def data_migration():
+
+    # Создаем спарк сессию с драйвером postgresql-42.7.3.jar
+    spark = SparkSession.builder \
+        .appName("PostgreSQL Connection with PySpark") \
+        .config("spark.jars", "/opt/spark/jars/postgresql-42.7.3.jar") \
+        .getOrCreate()
+
+    # Читаем sales_by_region_df из postgresql
+    sales_by_region_df = spark.read \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://host.docker.internal:5432/test") \
+        .option("dbtable", "sales_by_region") \
+        .option("user", "user") \
+        .option("password", "password") \
+        .option("driver", "org.postgresql.Driver") \
+        .load()
+    print("Данные прочитаны")
+
+    # Сохраняем в виде csv на хосте:
+    sales_by_region_df.write.mode('overwrite').options(header='True').csv('/opt/files/sales_by_region.csv')
+    # Останавливаем спарк сессию
+    spark.stop()
+
+    # Создаем спарк сессию с драйвером clickhouse-data-0.7.0.jar
+    spark = SparkSession.builder \
+        .appName("PostgreSQL Connection with PySpark") \
+        .config("spark.jars", "/opt/spark/jars/clickhouse-jdbc-0.7.0.jar") \
+        .getOrCreate()
+
+    # Получаем pyspark dataframe из csv файла
+    sales_region_df = spark.read.csv('/opt/files/sales_by_region.csv', header=True, inferSchema=True)
+
+    # Записываем в ClickHouse:
+    sales_region_df.write.mode("overwrite") \
+        .format("jdbc") \
+        .option("url", "jdbc:clickhouse://host.docker.internal:8123/default") \
+        .option("dbtable", "sales_by_region") \
+        .option("driver", "ru.yandex.clickhouse.ClickHouseDriver") \
+        .save()
+
+    print("Данные загружены")
+    spark.stop()
 
 task_data_generation = PythonOperator(
     task_id='data_generation',
@@ -151,5 +208,10 @@ task_conn_and_extract_postgresql = PythonOperator(
     dag=dag
 )
 
+task_data_migration = PythonOperator(
+    task_id="data_migration",
+    python_callable=data_migration,
+    dag=dag
+)
 
-task_data_generation >> task_query_pyspark >> task_conn_and_load_postgresql >> task_conn_and_extract_postgresql
+task_data_generation >> task_query_pyspark >> task_conn_and_load_postgresql >> task_conn_and_extract_postgresql >> task_data_migration
